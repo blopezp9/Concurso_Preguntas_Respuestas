ba8ccceeee14a9d04bcd50a75d733d3b004d75a9
blopezp9 <blopezp@eafit.edu.co>
2022-03-17 22:28:20 -0500

    Se implementaron los diferentes metodos de Juego en la clase servicioJuegoImpl

--- src/main/java/reto/tecnico/concurso_preguntas_respuestas/servicios/servicioJuego.java
+++ src/main/java/reto/tecnico/concurso_preguntas_respuestas/servicios/servicioJuego.java
@@ -1,4 +1,16 @@
 package reto.tecnico.concurso_preguntas_respuestas.servicios;
 
+import reto.tecnico.concurso_preguntas_respuestas.entidad.Juego;
+import reto.tecnico.concurso_preguntas_respuestas.entidad.Jugador;
+
+import java.util.Date;
+import java.util.List;
+
 public interface servicioJuego {
+
+    public List<Jugador> listJuegos();
+    public Juego getJuego(Long id);
+    public Juego crearJuego(String nombre_juego, Date Fecha_Juego);
+    public Juego actualizarJuego(Long id, Integer puntaje, Integer ronda, String estado);
+
 }
