793591d63f9b62b252caa4d999b7a1d6dd3c6926
blopezp9 <blopezp@eafit.edu.co>
2022-03-17 19:15:56 -0500

    Creación del proyecto con Java Spring Boot y Maven

---
+++ src/main/java/reto/tecnico/concurso_preguntas_respuestas/ConcursoPreguntasRespuestasApplication.java
@@ -0,0 +1,13 @@
+package reto.tecnico.concurso_preguntas_respuestas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcursoPreguntasRespuestasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConcursoPreguntasRespuestasApplication.class, args);
+    }
+
+}
---
+++ src/test/java/reto/tecnico/concurso_preguntas_respuestas/ConcursoPreguntasRespuestasApplicationTests.java
@@ -0,0 +1,13 @@
+package reto.tecnico.concurso_preguntas_respuestas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcursoPreguntasRespuestasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
