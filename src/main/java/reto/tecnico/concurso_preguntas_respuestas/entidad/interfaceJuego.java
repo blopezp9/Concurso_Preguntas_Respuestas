6cfc3bc2a1306d33f1fc05604ec5a7c5f71417db
blopezp9 <blopezp@eafit.edu.co>
2022-03-17 21:15:17 -0500

    Creación de la clase tipo Interface para la entidad Juego

---
+++ src/main/java/reto/tecnico/concurso_preguntas_respuestas/entidad/interfaceJuego.java
@@ -0,0 +1,8 @@
+package reto.tecnico.concurso_preguntas_respuestas.entidad;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface interfaceJuego extends JpaRepository<Jugador, Long> {
+    
+
+}
